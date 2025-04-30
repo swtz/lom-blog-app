1b9e5c9b797291087025604762437425c6cc7c79
Leonardo Lüders <luderser@hotmail.com>
2025-04-30 14:22:50 -0300

    Finished class 109 — Design Pattern Repository for Posts — Part 2

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,14 +1,31 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
+import { readFile } from 'fs/promises';
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = `${ROOT_DIR}/src/db/seed/posts.json`;
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
 
-  async findAll(): Promise<PostModel[]> {}
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+
+    if (!post) throw new Error('Post não encontrado');
+
+    return post;
+  }
 }
 
-export const postRepository = new JsonPostRepository();
-
-console.log(ROOT_DIR);
+export const postRepository: PostRepository = new JsonPostRepository();
--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -2,4 +2,5 @@ import { PostModel } from '@/models/post/post-model';
 
 export interface PostRepository {
   findAll(): Promise<PostModel[]>;
+  findById(id: string): Promise<PostModel>;
 }
