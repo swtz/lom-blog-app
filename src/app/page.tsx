ac72b5cbfcbcc805f2be901264d05f1616f90c6e
Leonardo Lüders <luderser@hotmail.com>
2025-05-01 12:11:23 -0300

    Finished class 119 — Created the PostCoverImage component using the Next.js Link and Image components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import { Suspense } from 'react';
+import clsx from 'clsx';
+
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostCoverImage } from '@/components/PostCoverImage';
 import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import clsx from 'clsx';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
@@ -16,19 +16,16 @@ export default async function HomePage() {
       <section
         className={clsx('grid grid-cols-1 gap-8 mb-16', 'sm:grid-cols-2 group')}
       >
-        <Link
-          className='w-full h-full overflow-hidden object-right rounded-2xl'
-          href='#'
-        >
-          <Image
-            className='group-hover:scale-105 w-full h-full object-cover object-center transition'
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt='Título do post'
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{ href: '/post/post-1' }}
+          imageProps={{
+            src: '/images/bryen_5.png',
+            alt: 'A imagem que mostra a capa do post',
+            width: 1200,
+            height: 720,
+            priority: true,
+          }}
+        />
         <div
           className={clsx(
             'flex flex-col gap-3',
