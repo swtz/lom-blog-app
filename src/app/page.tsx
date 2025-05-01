776805860bb60e109c1f48d4eb3f765603de0504
Leonardo Lüders <luderser@hotmail.com>
2025-04-30 21:57:36 -0300

    Finished class 116 — Initialized the FeaturedPost Component using Tailwind CSS Grid.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,35 @@ import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section
+        className={clsx('grid grid-cols-1 gap-8 mb-16', 'sm:grid-cols-2 group')}
+      >
+        <Link className='w-full h-full overflow-hidden rounded-2xl' href='#'>
+          <Image
+            className='group-hover:scale-105 transition'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='Título do post'
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          deleniti officiis, dicta suscipit mollitia harum voluptatibus illo.
+          Odio suscipit illum cumque placeat nihil dolorem sequi, omnis
+          laboriosam esse nulla. Neque.
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
