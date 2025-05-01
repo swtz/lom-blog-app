fc288f916405d1559e1851ed6c67c2b2b0c5ae86
Leonardo Lüders <luderser@hotmail.com>
2025-04-30 22:28:26 -0300

    Finished class 117 — Completed FeaturedPost CSS using TailWind CSS.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,20 +15,47 @@ export default async function HomePage() {
       <section
         className={clsx('grid grid-cols-1 gap-8 mb-16', 'sm:grid-cols-2 group')}
       >
-        <Link className='w-full h-full overflow-hidden rounded-2xl' href='#'>
+        <Link
+          className='w-full h-full overflow-hidden object-right rounded-2xl'
+          href='#'
+        >
           <Image
-            className='group-hover:scale-105 transition'
+            className='group-hover:scale-105 w-full h-full object-cover object-center transition'
             src='/images/bryen_0.png'
             width={1200}
             height={720}
             alt='Título do post'
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-          deleniti officiis, dicta suscipit mollitia harum voluptatibus illo.
-          Odio suscipit illum cumque placeat nihil dolorem sequi, omnis
-          laboriosam esse nulla. Neque.
+        <div
+          className={clsx(
+            'flex flex-col gap-3',
+            'sm:justify-center sm:gap-2.5',
+          )}
+        >
+          <time
+            className='text-slate-600 block text-sm/tight'
+            dateTime='2025-04-30'
+          >
+            30/04/2025 21:59
+          </time>
+
+          <h1
+            className={clsx(
+              'text-2xl/tight font-extrabold',
+              'sm:text-3xl',
+              'md:text-4xl',
+            )}
+          >
+            <Link href='#'>Lorem ipsum dolor sit amet.</Link>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            deleniti officiis, dicta suscipit mollitia harum voluptatibus illo.
+            Odio suscipit illum cumque placeat nihil dolorem sequi, omnis
+            laboriosam esse nulla. Neque.
+          </p>
         </div>
       </section>
 
