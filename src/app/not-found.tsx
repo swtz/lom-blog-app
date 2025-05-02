59c6931f00258a3d0bb2654b8d10feef23d7568f
Leonardo Lüders <luderser@hotmail.com>
2025-05-02 20:27:52 -0300

    Finished class 132 — Used Next.js generateMetadata() to dynamically generate title, description, and other meta information.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,21 @@ import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-[320px] bg-slate-900 text-slate-100',
-        'mb-16 p-8 rounded-xl',
-        'flex flex-col item-center justify-center text-center',
-      )}
-    >
-      <div>
-        <h1 className='text-4xl'>Página não encontrada</h1>
-        <p className='text-3xl/normal font-bold mb-8'>Erro 404</p>
-        <p>Essa página não existe ou foi removida desse site.</p>
+    <>
+      <title>Página não encontrada</title>
+      <div
+        className={clsx(
+          'min-h-[320px] bg-slate-900 text-slate-100',
+          'mb-16 p-8 rounded-xl',
+          'flex flex-col item-center justify-center text-center',
+        )}
+      >
+        <div>
+          <h1 className='text-4xl'>Página não encontrada</h1>
+          <p className='text-3xl/normal font-bold mb-8'>Erro 404</p>
+          <p>Essa página não existe ou foi removida desse site.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
