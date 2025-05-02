10c5ad14dce1c39cb6b12aeea88fdd16d36cf9f6
Leonardo Lüders <luderser@hotmail.com>
2025-05-02 14:48:08 -0300

    Finished class 129 — Created a dynamic route in Next.js.

--- src/app/about/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function AboutPage() {
-  return <h1>Essa aqui é a AboutPage</h1>;
-}
---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,9 @@
+type PostSlugPageProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export default async function PostSlugPage({ params }: PostSlugPageProps) {
+  const { slug } = await params;
+
+  return <h1>Hello, dynamic route: {slug}</h1>;
+}
