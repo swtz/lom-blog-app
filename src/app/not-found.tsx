b7f5d239e849dc30c69663627aaac4b078b0691d
Leonardo Lüders <luderser@hotmail.com>
2025-05-05 14:16:35 -0300

    Finished class 138 — Created the ErrorMessage component to display a error message to user.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,11 @@
-import clsx from 'clsx';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div
-        className={clsx(
-          'min-h-[320px] bg-slate-900 text-slate-100',
-          'mb-16 p-8 rounded-xl',
-          'flex flex-col item-center justify-center text-center',
-        )}
-      >
-        <div>
-          <h1 className='text-4xl'>Página não encontrada</h1>
-          <p className='text-3xl/normal font-bold mb-8'>Erro 404</p>
-          <p>Essa página não existe ou foi removida desse site.</p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle='Página não encontrada'
+      contentTitle='Erro 404'
+      content='A página que você procurou não existe ou foi removida do site.'
+    />
   );
 }
