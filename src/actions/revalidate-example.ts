4ae13befb50c321fef9f435b5800a87a7e50926b
Leonardo Lüders <luderser@hotmail.com>
2025-05-06 16:44:51 -0300

    Finished class 156 — Introduction to the Server Actions using the 'use server' directive.

---
+++ src/actions/revalidate-example.ts
@@ -0,0 +1,6 @@
+'use server';
+
+export async function revalidateExampleAction(formData: FormData) {
+  const path = formData.get('path') || '';
+  console.log('Estou em uma server action.', path);
+}
--- src/app/exemplo/[id]/page.tsx
+++ src/app/exemplo/[id]/page.tsx
@@ -1,11 +1,8 @@
+import { revalidateExampleAction } from '@/actions/revalidate-example';
 import { formatHour } from '@/utils/format-datetime';
 
 export const dynamic = 'force-static';
-export const revalidate = 10;
-
-// export async function generateStaticParams() {
-//   return [{ id: '1' }, { id: '2' }];
-// }
+// export const revalidate = 10;
 
 export default async function ExemploDynamicPage({
   params,
@@ -20,6 +17,17 @@ export default async function ExemploDynamicPage({
       <div>
         Hora: {hour} ID: {id}
       </div>
+
+      <form className='py-16' action={revalidateExampleAction}>
+        <input type='hidden' name='path' defaultValue={`/exemplo/${id}`} />
+
+        <button
+          className='bg-blue-600 text-white p-2 rounded-4xl hover:bg-blue-300 transition cursor-pointer'
+          type='submit'
+        >
+          Revalidate
+        </button>
+      </form>
     </main>
   );
 }
