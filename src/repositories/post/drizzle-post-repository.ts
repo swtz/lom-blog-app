241ce5e62fa2e3a20662f1d8d1ce71dc72ae6500
Leonardo Lüders <luderser@hotmail.com>
2025-05-06 11:41:54 -0300

    Finished class 150 — Completed the DrizzlePostRepository implementation.

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,6 +1,7 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
 import { drizzleDb } from '@/db/drizzle';
+import { eq } from 'drizzle-orm';
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
@@ -11,14 +12,46 @@ export class DrizzlePostRepository implements PostRepository {
 
     return posts;
   }
-  async findAll(): Promise<PostModel[]> {}
-  async findById(id: string): Promise<PostModel> {}
-  async findBySlugPublic(slug: string): Promise<PostModel> {}
+  async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
+
+    if (!post) throw new Error('Post não encontrado pelo slug');
+
+    return post;
+  }
+  async findAll(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    });
+
+    return posts;
+  }
+  async findById(id: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+
+    if (!post) throw new Error('Post não encontrado pelo id');
+
+    return post;
+  }
 }
 
-(async () => {
-  const repo = new DrizzlePostRepository();
-  const posts = await repo.findAllPublic();
+// Testes:
 
-  posts.forEach(post => console.log(post.slug, post.published));
-})();
+// (async () => {
+//   const repo = new DrizzlePostRepository();
+//   const post = await repo.findById('be3f14a1-0105-4e2e-bfc9-133a05e7bda6');
+
+//   console.log(post.id);
+// })();
+
+// (async () => {
+//   const repo = new DrizzlePostRepository();
+//   const posts = await repo.findAllPublic();
+
+//   posts.forEach(post => console.log(post.author, post.published));
+// })();
