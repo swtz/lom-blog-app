f8ce4adfa48baa66decbf72d522784d68bb576dd
Leonardo Lüders <luderser@hotmail.com>
2025-05-07 17:42:19 -0300

    Finished class 161 — Created the admin routes for the website.

---
+++ src/app/admin/post/[id]/page.tsx
@@ -0,0 +1,19 @@
+export const dynamic = 'force-dynamic';
+
+type AdminPostIdPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function AdminPostIdPage({
+  params,
+}: AdminPostIdPageProps) {
+  const { id } = await params;
+
+  return (
+    <div className='py-16 text-6xl'>
+      Hello, Post ID Page {'->'} {id}
+    </div>
+  );
+}
---
+++ src/app/admin/post/new/page.tsx
@@ -0,0 +1,3 @@
+export default async function AdminPostNewPage() {
+  return <div className='py-16 text-6xl'>Hello, Post New Page!</div>;
+}
