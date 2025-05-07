d0e3a4be1050a911afe02caec3dee86d9613a131
Leonardo Lüders <luderser@hotmail.com>
2025-05-07 10:10:38 -0300

    Finished class 157 — Used revalidatePath() to manually revalidate the Next.js cache.

--- src/actions/revalidate-example.ts
+++ src/actions/revalidate-example.ts
@@ -1,6 +1,10 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
+
 export async function revalidateExampleAction(formData: FormData) {
   const path = formData.get('path') || '';
   console.log('Estou em uma server action.', path);
+
+  revalidatePath(`${path}`);
 }
