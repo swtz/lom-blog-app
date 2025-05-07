6ff01857417263b66a10e1d42fad2dd60810b409
Leonardo Lüders <luderser@hotmail.com>
2025-05-07 10:43:32 -0300

    Finished class 158 — Learned Data Fetching using the Fetch API with tags, revalidate and revalidateTags().

--- src/actions/revalidate-example.ts
+++ src/actions/revalidate-example.ts
@@ -1,10 +1,11 @@
 'use server';
 
-import { revalidatePath } from 'next/cache';
+import { revalidateTag } from 'next/cache';
 
 export async function revalidateExampleAction(formData: FormData) {
   const path = formData.get('path') || '';
   console.log('Estou em uma server action.', path);
 
-  revalidatePath(`${path}`);
+  // revalidatePath(`${path}`);
+  revalidateTag('randomuser');
 }
--- src/app/exemplo/[id]/page.tsx
+++ src/app/exemplo/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { revalidateExampleAction } from '@/actions/revalidate-example';
 import { formatHour } from '@/utils/format-datetime';
 
-export const dynamic = 'force-static';
+// export const dynamic = 'force-static';
 // export const revalidate = 10;
 
 export default async function ExemploDynamicPage({
@@ -12,10 +12,19 @@ export default async function ExemploDynamicPage({
   const { id } = await params;
   const hour = formatHour(Date.now());
 
+  const response = await fetch('https://randomuser.me/api/?results=1', {
+    next: {
+      tags: ['randomuser'],
+      revalidate: 30,
+    },
+  });
+  const json = await response.json();
+  const name = json.results[0].name.first;
+
   return (
     <main className='min-h-[600px] text-xl font-bold'>
       <div>
-        Hora: {hour} ID: {id}
+        Name: {name} | Hora: {hour} | ID: {id}
       </div>
 
       <form className='py-16' action={revalidateExampleAction}>
