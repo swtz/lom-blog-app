75a68a4bca27f68c6032eccf482dedf89975edfd
Leonardo Lüders <luderser@hotmail.com>
2025-05-08 13:25:23 -0300

    Finished class 169 — Added features for the Dialog component.

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -4,7 +4,7 @@ import { asyncDelay } from '@/utils/async-delay';
 import { logColor } from '@/utils/log-color';
 
 export async function deletePostAction(id: string) {
-  await asyncDelay(500);
+  await asyncDelay(2000);
 
   logColor(`${id}`);
   return id;
--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -29,54 +29,6 @@ export async function PostsListAdmin() {
           <DeletePostButton id={post.id} title={post.title} />
         </div>
       ))}
-      <div
-        className={clsx(
-          'fixed z-50 bg-black/50',
-          'inset-0', // top-0 bottom-0 left-0 right-0
-          'backdrop-blur-xs',
-          'flex items-center justify-center',
-        )}
-      >
-        <div
-          className={clsx(
-            'bg-slate-100 p-6 rounded-lg max-w-2xl mx-6',
-            'flex flex-col gap-6',
-            'shadow-lg shadow-black/30',
-            'text-center',
-          )}
-        >
-          <h3 className='text-xl font-extrabold'>Título do dialog</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            animi accusantium pariatur quod hic! Rem nulla vitae magni ipsam,
-            non a exercitationem earum praesentium, beatae, adipisci quae
-            consequatur quasi quaerat.
-          </p>
-          <div className='flex items-center justify-around'>
-            <button
-              className={clsx(
-                'bg-slate-300 hover:bg-slate-400 transition text-slate-950',
-                'flex items-center justify-center',
-                'py-2 px-4',
-                'rounded-lg cursor-pointer',
-              )}
-              autoFocus
-            >
-              Cancelar
-            </button>
-            <button
-              className={clsx(
-                'bg-blue-500 hover:bg-blue-600 transition text-blue-50',
-                'flex items-center justify-center',
-                'py-2 px-4',
-                'rounded-lg cursor-pointer',
-              )}
-            >
-              Ok
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
