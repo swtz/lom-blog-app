1aee6f67a2b8304afc956ce5d4fbcd602ce3bd00
Leonardo Lüders <luderser@hotmail.com>
2025-05-08 10:35:30 -0300

    Finished class 167 — Used a server action with the useTransition hook inside a client component.

--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,9 +1,11 @@
 'use server';
 
+import { asyncDelay } from '@/utils/async-delay';
 import { logColor } from '@/utils/log-color';
 
-export async function deletePostAction(formData: FormData) {
-  const id = formData.get('id');
+export async function deletePostAction(id: string) {
+  await asyncDelay(500);
 
   logColor(`${id}`);
+  return id;
 }
