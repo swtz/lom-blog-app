c428ace10f0ae79236cb9b9fd13dd4d621f33f20
Leonardo Lüders <luderser@hotmail.com>
2025-05-09 16:22:52 -0300

    Finished class 175 — Discussion about creating components that extend native JSX elements.

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/Button';
+
 export default async function AdminPostNewPage() {
-  return <div className='py-16 text-6xl'>Hello, Post New Page!</div>;
+  return (
+    <div className='py-16 text-6xl'>
+      Hello, Post New Page!
+      <Button>Olá, mundo!</Button>
+    </div>
+  );
 }
