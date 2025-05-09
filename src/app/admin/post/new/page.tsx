f7d2c17bc4992f890f522136c33152f7174d81b9
Leonardo Lüders <luderser@hotmail.com>
2025-05-09 17:32:08 -0300

    Finished class 176 — Used TypeScript to define styled variants for the Button component.

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -2,9 +2,16 @@ import { Button } from '@/components/Button';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16 text-6xl'>
-      Hello, Post New Page!
-      <Button>Olá, mundo!</Button>
+    <div className='py-16 flex gap-4'>
+      <Button variant='default' size='sm'>
+        Olá, mundo!
+      </Button>
+      <Button variant='ghost' size='md'>
+        Olá, mundo!
+      </Button>
+      <Button variant='danger' size='lg'>
+        Olá, mundo!
+      </Button>
     </div>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,32 @@
+import clsx from 'clsx';
 import { ComponentProps } from 'react';
 
-type ButtonProps = {} & ComponentProps<'button'>;
+type ButtonVariants = 'default' | 'ghost' | 'danger';
+type ButtonSizes = 'sm' | 'md' | 'lg';
 
-export function Button({ ...props }: ButtonProps) {
-  return <button {...props} />;
+type ButtonProps = {
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
+} & ComponentProps<'button'>;
+
+export function Button({
+  variant = 'default',
+  size = 'md',
+  ...props
+}: ButtonProps) {
+  const buttonVariants: Record<ButtonVariants, string> = {
+    default: clsx('bg-blue-600 text-blue-100'),
+    ghost: clsx('bg-slate-200 text-slate-900'),
+    danger: clsx('bg-red-600 text-red-100'),
+  };
+
+  const buttonSizes: Record<ButtonSizes, string> = {
+    sm: clsx(''),
+    md: clsx(''),
+    lg: clsx(''),
+  };
+
+  const buttonClasses = clsx(buttonVariants[variant], buttonSizes[size]);
+
+  return <button className={buttonClasses} {...props} />;
 }
