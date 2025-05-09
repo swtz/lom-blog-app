965385a252d8796aa3999517c1da389b4e37f4db
Leonardo Lüders <luderser@hotmail.com>
2025-05-09 15:23:06 -0300

    Finished class 174 — Implemented open/close logic for MenuAdmin using useState, useEffect, and usePathname.

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,26 +1,68 @@
+'use client';
+
 import Link from 'next/link';
-import { FileTextIcon, HouseIcon } from 'lucide-react';
+import {
+  CircleXIcon,
+  FileTextIcon,
+  HouseIcon,
+  MenuIcon,
+  PlusIcon,
+} from 'lucide-react';
 import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 export function MenuAdmin() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   const navClasses = clsx(
     'bg-slate-900 text-slate-100 rounded-lg',
     'flex flex-col',
     'mb-8',
     'sm:flex-row sm:flex-wrap',
-    // 'h-10',
-    // 'overflow-hidden',
+    !isOpen && 'h-10',
+    !isOpen && 'overflow-hidden',
+    'sm:overflow-auto sm:h-auto',
   );
   const linkClasses = clsx(
     '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
-    'flex items-center gap-2',
+    'flex items-center gap-2 cursor-pointer',
     'transition hover:bg-slate-800 rounded-lg',
     'h-10',
     'shrink-0',
   );
+  const openCloseBtnClasses = clsx(
+    linkClasses,
+    'text-blue-200 italic',
+    'sm:hidden',
+  );
 
   return (
     <nav className={navClasses}>
+      <button
+        onClick={() => setIsOpen(s => !s)}
+        className={openCloseBtnClasses}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
+
       <a className={linkClasses} href='/' target='_blank'>
         <HouseIcon />
         Home
@@ -29,6 +71,11 @@ export function MenuAdmin() {
       <Link className={linkClasses} href='/admin/post'>
         <FileTextIcon />
         Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/post/new'>
+        <PlusIcon />
+        Criar Post
       </Link>
     </nav>
   );
