46570b7d84966e1795f88404155557c109917678
Leonardo Lüders <luderser@hotmail.com>
2025-05-10 09:58:19 -0300

    Finished class 178 — Created the InputText component for the form.

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,38 +1,32 @@
-import { Button } from '@/components/Button';
-import { BugIcon } from 'lucide-react';
+import { InputText } from '@/components/InputText';
 
 export default async function AdminPostNewPage() {
   return (
-    <>
-      <div className='py-16 flex gap-4 items-center'>
-        <Button variant='default' size='sm'>
-          <BugIcon />
-          Olá, mundo!
-        </Button>
-        <Button variant='ghost' size='md'>
-          <BugIcon />
-          Olá, mundo!
-        </Button>
-        <Button variant='danger' size='lg'>
-          <BugIcon />
-          Olá, mundo!
-        </Button>
-      </div>
-
-      <div className='py-16 flex gap-4 items-center'>
-        <Button disabled variant='default' size='sm'>
-          <BugIcon />
-          Olá, mundo!
-        </Button>
-        <Button disabled variant='ghost' size='md'>
-          <BugIcon />
-          Olá, mundo!
-        </Button>
-        <Button disabled variant='danger' size='lg'>
-          <BugIcon />
-          Olá, mundo!
-        </Button>
-      </div>
-    </>
+    <div className='flex flex-col gap-6'>
+      <InputText labelText='Nome' placeholder='Digite seu nome' />
+      <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
+      <InputText
+        disabled
+        labelText='Sobrenome'
+        placeholder='Digite seu sobrenome'
+        defaultValue={'Estou desabilitado!'}
+      />
+      <InputText
+        disabled
+        labelText='desabilitado sem defaultValue'
+        placeholder='Digite seu sobrenome'
+      />
+      <InputText
+        labelText='Sobrenome'
+        placeholder='Digite seu sobrenome'
+        defaultValue={'Sou somente leitura!'}
+        readOnly
+      />
+      <InputText
+        labelText='Sobrenome'
+        placeholder='readOnly sem defaultValue'
+        readOnly
+      />
+    </div>
   );
 }
