3ea7a70eca12501d3bdf1d58ea6b75bcc9ae7610
Leonardo Lüders <luderser@hotmail.com>
2025-05-13 21:21:55 -0300

    Finished class 181 — Created the MarkdownEditor component using @uiw/react-md-editor package.

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { useState } from 'react';
 import { Button } from '@/components/Button';
 import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
+import { MarkdownEditor } from '@/components/MarkdownEditor';
 
 export function ManagePostForm() {
+  const [contentValue, setContentValue] = useState('');
+
   return (
     <form action='' className='mb-16'>
       <div className='flex flex-col gap-6'>
@@ -12,6 +16,14 @@ export function ManagePostForm() {
         <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
 
         <InputCheckbox labelText='custom checkbox' />
+
+        <MarkdownEditor
+          labelText='Conteúdo do post'
+          disabled={false}
+          textAreaName='content'
+          value={contentValue}
+          setValue={setContentValue}
+        />
 
         <div className='mt-4'>
           <Button type='submit'>Enviar</Button>
