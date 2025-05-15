bb75c44d862d00369a983c0d38b638349e1e62af
Leonardo Lüders <luderser@hotmail.com>
2025-05-15 11:06:52 -0300

    Finished class 183 — Created the handleChange method to validate the image size in frontend.

--- src/components/Admin/ImageUploader/index.tsx
+++ src/components/Admin/ImageUploader/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Button } from '@/components/Button';
+import { IMAGE_UPLOADER_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef } from 'react';
+import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -13,6 +15,35 @@ export function ImageUploader() {
     fileInputRef.current.click();
   }
 
+  function handleChange() {
+    toast.dismiss();
+
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0]; // existe 'files'? existe algo dentro de 'files'?
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOADER_MAX_SIZE / 1024;
+      toast.error(
+        `A imagem enviada é muito grande. Tamanho máximo: ${readableMaxSize}KB.`,
+      );
+
+      fileInput.value = ''; // caso o usuário envie a mesma imagem, o 'event onChange' não é provocado
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // TODO: criar a action para upload do arquivo
+    console.log(formData.get('file'));
+
+    fileInput.value = ''; // reset do campo 'input'
+  }
+
   return (
     <div className='flex flex-col gap-2 py-4'>
       <Button onClick={handleChooseFile} type='button' className='self-start'>
@@ -21,6 +52,7 @@ export function ImageUploader() {
       </Button>
 
       <input
+        onChange={handleChange}
         ref={fileInputRef}
         className='hidden'
         name='file'
