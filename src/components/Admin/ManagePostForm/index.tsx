8d36893d0df520f98ca2cec7adc06b8adbe7740c
Leonardo Lüders <luderser@hotmail.com>
2025-05-16 10:29:44 -0300

    Finished class 188 — Completed the ManagePostForm structure.

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -7,26 +7,85 @@ import { InputText } from '@/components/InputText';
 import { MarkdownEditor } from '@/components/MarkdownEditor';
 import { ImageUploader } from '../ImageUploader';
 
+//   [src/models/post/post-model.ts]
+//
+//   id: string; gerado automaticamente
+//   slug: string; gerado automaticamente
+//   author: string;
+//   title: string;
+//   excerpt: string;
+//   content: string;
+//   UPLOADER
+//   coverImageUrl: string;
+//   published: boolean;
+
 export function ManagePostForm() {
   const [contentValue, setContentValue] = useState('');
 
   return (
     <form action='' className='mb-16'>
       <div className='flex flex-col gap-6'>
-        <InputText labelText='Nome' placeholder='Digite seu nome' />
-        <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
+        <InputText
+          labelText='ID'
+          name='ID'
+          placeholder='ID gerado automaticamente'
+          type='text'
+          defaultValue={''}
+          readOnly
+        />
 
-        <ImageUploader />
+        <InputText
+          labelText='Slug'
+          name='slug'
+          placeholder='Slug gerado automaticamente'
+          type='text'
+          defaultValue={''}
+          readOnly
+        />
 
-        <InputCheckbox labelText='custom checkbox' />
+        <InputText
+          labelText='Autor'
+          name='author'
+          placeholder='Digite o nome do autor'
+          type='text'
+          defaultValue={''}
+        />
+
+        <InputText
+          labelText='Título'
+          name='title'
+          placeholder='Digite o título'
+          type='text'
+          defaultValue={''}
+        />
+
+        <InputText
+          labelText='Excerto'
+          name='excerpt'
+          placeholder='Digite a descrição'
+          type='text'
+          defaultValue={''}
+        />
 
         <MarkdownEditor
-          labelText='Conteúdo do post'
-          disabled={false}
+          labelText='Conteúdo'
           textAreaName='content'
           value={contentValue}
           setValue={setContentValue}
+          disabled={false}
         />
+
+        <ImageUploader />
+
+        <InputText
+          labelText='URL da imagem de capa'
+          name='coverImageUrl'
+          placeholder='Digite a descrição'
+          type='text'
+          defaultValue={''}
+        />
+
+        <InputCheckbox labelText='Publicar?' name='published' type='checkbox' />
 
         <div className='mt-4'>
           <Button type='submit'>Enviar</Button>
