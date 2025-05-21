8135a922ad7af3e0cdf1841e118e54f12ff120d4
Leonardo Lüders <luderser@hotmail.com>
2025-05-20 22:34:24 -0300

    Finished class 204 — Generated and compared password hashes using the Bcrypt library for security.

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -13,6 +13,19 @@ export async function loginAction(
 ) {
   await asyncDelay(5000); // manter delay
 
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados inválidos',
+    };
+  }
+
+  const username = formData.get('username')?.toString() || '';
+  const password = formData.get('password')?.toString() || '';
+
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = '';
+
   return {
     username: '',
     error: 'error testing',
